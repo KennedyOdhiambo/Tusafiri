e327985f750a29c5a555b0b2767c1297dcfe58ab
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-05 16:27:29 +0300

    persist user selection while navigating to /bookings

--- src/features/trips/SearchButton.tsx
+++ src/features/trips/SearchButton.tsx
@@ -1,26 +1,13 @@
-'use client';
 import { Button } from '@/components/ui/button';
-import { TripsContext } from '@/context/TripsContext';
-import React, { useContext } from 'react';
+import Link from 'next/link';
+import React from 'react';
 
 export default function SearchButton() {
-  const tripsContext = useContext(TripsContext);
-  const departure = tripsContext?.departure;
-  const destination = tripsContext?.destination;
-  const travelDate = tripsContext?.travelDate;
-
-  const handleButtonClick = () => {
-    const tripParams = { departure, destination, travelDate };
-    console.log(tripParams);
-  };
-
   return (
-    <Button
-      onClick={handleButtonClick}
-      variant={'default'}
-      className="mt-2 w-[240px] self-end xl:w-[180px] xl:justify-self-end"
-    >
-      Find Bus
-    </Button>
+    <Link href={'/booking'} className="mt-2 w-[240px] self-end xl:w-[180px] xl:justify-self-end">
+      <Button variant={'default'} className="w-full">
+        Find Bus
+      </Button>
+    </Link>
   );
 }
