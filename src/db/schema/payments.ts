552c9fe1363a71a710e6e1a88c3ebd7732fd6cd5
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-11 18:09:32 +0300

    more unused vars

--- src/db/schema/payments.ts
+++ src/db/schema/payments.ts
@@ -1,10 +1,10 @@
-import { pgTable, serial, uuid, varchar } from 'drizzle-orm/pg-core';
-import { bookings } from './bookings';
-import { users } from './users';
+import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
+import { bookings } from './bookings'
+import { users } from './users'
 
 export const payments = pgTable('payments', {
   paymentId: uuid('payment_id').primaryKey().defaultRandom(),
   bookingId: uuid('bookingId').references(() => bookings.bookingId),
   userId: uuid('user_id').references(() => users.userId),
   phoneNumber: varchar('payment_phone', { length: 256 }).notNull(),
-});
+})
