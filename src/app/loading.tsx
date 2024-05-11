cf86fa5c878bf832ef87ce3f5ff1765f57c79868
Kennedy <x.johnkennedy.x@gmail.com>
2024-05-11 18:07:18 +0300

    remove unused vars

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,44 +1,44 @@
-'use client';
+'use client'
 
-import { Progress } from '@/components/ui/progress';
-import { usePathname } from 'next/navigation';
-import React, { useEffect, useState, useTransition } from 'react';
+import { Progress } from '@/components/ui/progress'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState, useTransition } from 'react'
 
 export default function Loading() {
-  const pathName = usePathname();
-  const [isPending, startTransition] = useTransition();
-  const [progress, setProgress] = useState(0);
+  const pathName = usePathname()
+  const [, startTransition] = useTransition()
+  const [progress, setProgress] = useState(0)
 
   useEffect(() => {
-    let progressInterval: ReturnType<typeof setInterval>;
+    let progressInterval: ReturnType<typeof setInterval>
 
     const updateProgress = () => {
       setProgress((prev) => {
-        const newProgress = prev + 10;
+        const newProgress = prev + 10
         if (newProgress >= 100) {
-          clearInterval(progressInterval);
-          return 100;
+          clearInterval(progressInterval)
+          return 100
         }
 
-        return newProgress;
-      });
-    };
+        return newProgress
+      })
+    }
 
     startTransition(() => {
-      setProgress(0);
-      progressInterval = setInterval(updateProgress, 100);
-    });
+      setProgress(0)
+      progressInterval = setInterval(updateProgress, 100)
+    })
 
     return () => {
       if (progressInterval !== null) {
-        clearInterval(progressInterval);
+        clearInterval(progressInterval)
       }
-    };
-  }, [pathName, startTransition]);
+    }
+  }, [pathName, startTransition])
 
   return (
     <div>
       <Progress value={progress} />
     </div>
-  );
+  )
 }
